ea345ed438e8132acaea7ff71ef88a2203756dca
Kyle <prophet@hanyang.ac.kr>
2023-12-04 01:17:10 +0900

    함수와 이벤트 - 함수 알아보기

---
+++ Do it!/15/js/no-function.js
@@ -0,0 +1,8 @@
+// 함수를 사용하지 않고 두 수 더하기
+'use strict'
+
+let num1 = 2
+let num2 = 3
+let sum = num1 + num2
+
+console.log('결괏값:', sum)
---
+++ Do it!/15/js/using-function.js
@@ -0,0 +1,13 @@
+// 함수를 사용해 두 수 더하기
+'use strict'
+
+addNumber() // 함수 선언 위치와 상관없이 함수 실행 가능
+
+function addNumber() {
+    let num1 = 2
+    let num2 = 3
+    let sum = num1 + num2
+    console.log('결괏값:', sum)
+}
+
+addNumber()
