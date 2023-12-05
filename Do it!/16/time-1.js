cb3d80e359a7949c580c9c235d03a8dba163dd93
Kyle <prophet@hanyang.ac.kr>
2023-12-06 00:47:40 +0900

    자바스크립트와 객체 - 객체 알아보기

---
+++ Do it!/16/time-1.js
@@ -0,0 +1,5 @@
+/* Date 객체의 인스턴스 만들기 */
+'use strict'
+
+let now = new Date()    // Date 인스턴스 만들고 변수에 저장하기
+console.log(`현재 시각은 ${now}입니다.`)
---
+++ Do it!/16/time-2.js
@@ -0,0 +1,5 @@
+/* 로컬 형식으로 현재 시각 표시하기 */
+'use strict'
+
+let now = new Date()
+console.log(`현재 시각은 ${now.toLocaleString()}입니다.`)
